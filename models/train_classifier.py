19ac9263df7e024945cf775574b26b707cfd913a
Subhash Naveen <naveen.vijapurapu@gmail.com>
2019-08-16 23:49:11 +0530

    Update train_classifier.py
    Commented and doc strings for functions

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,30 +1,39 @@
 
 # How to run. E.g. below
-# > python train_classifier.py ../data/DisasterResponse.db classifier.pkl
+# > python train_classifier.py data/DisasterResponse.db models/classifier.pkl
 
 import sys
 import pandas as pd
 import numpy as np
 import pickle
 import re
+
 from sqlalchemy import create_engine
+
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.model_selection import train_test_split
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.metrics import fbeta_score, classification_report
+
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
-from sklearn.pipeline import Pipeline, FeatureUnion
-from sklearn.metrics import fbeta_score, classification_report
-from scipy.stats.mstats import gmean
 from nltk.corpus import stopwords
-
 nltk.download(['punkt', 'wordnet', 'stopwords', 'averaged_perceptron_tagger'])
-
+from scipy.stats.mstats import gmean
 
 def load_data(database_filepath):
+    '''
+    FUNC
+        As the name suggests, this function is to load the dat
+    INPUT
+        databasefilepath
+    OUTPUT
+        X feature matrix, y target vector & category names
+    '''
     engine = create_engine('sqlite:///'+ database_filepath)
     df = pd.read_sql_table('df', engine)
     X = df['message']
@@ -34,22 +43,35 @@ def load_data(database_filepath):
 
 
 def tokenize(text):
+    '''
+    FUNC
+        Tokenize custom function. It involves multiple steps:
+        Step1: Replace All URL's with 'urlplaceholder' text
+        Step2: Call word_tokenize method on the text to convert to individual tokens
+        Step3: Instantiate WordNet Lemmatizer, which converts tokens to their root form and lower the case
         
+    INPUT
+        text for tokenizing, lemmatizing & lower case conversion
+    OUTPUT
+        clean tokens
+    '''        
+    
     # the below will help idenitfy any possible URL's in the text
     url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
     
     detected_urls = re.findall(url_regex, text)
     
+    # replace URL with static text - "urlplaceholder"
     for url in detected_urls:
         text = text.replace(url, "urlplaceholder")
     
     # tokenize the text
     tokens =  word_tokenize(text)
 
-  
-    # lemmetize the tokens to root form e.g. rides, riding to 'ride'
+    # Instantiate Lemmatizer
     lem = WordNetLemmatizer()
     
+    # loop through the tokens and lemmetize to root form e.g. rides, riding to 'ride'
     clean_tokens = []
     for tok in tokens:
         clean_tok = lem.lemmatize(tok).lower().strip()
@@ -62,16 +84,22 @@ class StartingVerbExtractor(BaseEstimator, TransformerMixin):
     """
     Starting Verb Extractor class
     
-    This class extract the starting verb of a sentence,
+    This class extracts the starting verb of a sentence,
     creating a new feature for the ML classifier
     """
 
     def starting_verb(self, text):
-        
+        # sentence tokenize
         sentence_list = nltk.sent_tokenize(text)
+        
+        #loop through sentence list
         for sentence in sentence_list:
+            # Parts of Speech Tagging using NLTK pos_tag method
             pos_tags = nltk.pos_tag(tokenize(sentence))
+            
             first_word, first_tag = pos_tags[0]
+            
+            # VB, VBP--> Verb base form and singular
             if first_tag in ['VB', 'VBP'] or first_word == 'RT':
                 return True
         return False
@@ -84,6 +112,11 @@ class StartingVerbExtractor(BaseEstimator, TransformerMixin):
         return pd.DataFrame(X_tagged)
 
 def build_model():
+    '''
+    Model building using Pipeline and Feature Union
+    CountVectorizer, TFIDF and MultiOutput Classifier are run here sequentially
+    '''
+    
     model = Pipeline([
         ('features', FeatureUnion([
             
@@ -120,6 +153,13 @@ def multioutput_fscore(y_true,y_pred,beta=1):
     return  f1score
 
 def evaluate_model(model, X_test, Y_test, category_names):
+    '''
+    Model Evaluation
+    INPUT
+        model, X_test, Y_test, categories
+    OUTPUT
+        Prints Accuracy and F1 scores
+    '''
     Y_pred = model.predict(X_test)
     multi_f1 = multioutput_fscore(Y_test,Y_pred, beta = 1)
     overall_accuracy = (Y_pred == Y_test).mean().mean()
